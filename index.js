d897c6544d45db9db2daf06fea8f9596b39db64d
Chen Ye <dpp0921@gmail.com>
2018-01-09 02:10:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+function increaseRankBy(n){
+  const rankList = document.querySelectorAll('.ranked-list');
+  for(let i = 0, l = rankList.length; i < l; i++){
+    let children = rankList[i].children;
+    for(let j = 0, k = children.length; j < k; j++){
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
+  }
+}
+function deepestChild(){
+  
+}
